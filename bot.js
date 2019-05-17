44c8a721ac6b6c303db9462147860e1116ca02e2
mikimasn <50592309+mikimasn@users.noreply.github.com>
2019-05-17 15:55:02 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,37 @@
+
+const Discord = require("discord.js");
+const auth = require("./auth.json");
+const Words = ["fak","kurwa","shit"];
+const client = new Discord.Client();
+const users = [];
+var dates = Date();
+var timeh = dates;
+var timem = dates;
+const times = dates;
+var timessplit;
+var howerssplit
+client.on("ready", () => {
+ console.log("I am ready!");
+});
+client.on("message", (message) => { dates = Date();
+                                   console.log(dates);
+                                   timessplit = dates.split(" ");
+                                   howerssplit = timessplit[4].split(":");
+                                   console.log(timessplit[4]);
+                                   console.log(howerssplit[0]);
+                                   console.log(howerssplit[1]);
+                                   console.log(howerssplit[2]);
+                                   if(howerssplit[0] >= 22 || howerssplit[0] < 6)
+                                       {
+                                           if (message.author.tag == client.user.tag)
+                                               {
+                                                   console.log("not");
+                                               }
+                                           else{
+                                                    message.delete();
+                                                    message.author.send("przypominam o ciszy nocnej :)");
+                                           }
+                                       }
+
+                                  })
+client.login(auth.token);
