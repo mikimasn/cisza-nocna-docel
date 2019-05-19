94176c5dd2dc02cf12cb9bf6f9464c56224382fa
mikimasn <50592309+mikimasn@users.noreply.github.com>
2019-05-19 09:23:53 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,75 @@
+
+const Discord = require("discord.js");
+const auth = require("./auth.json");
+const Words = ["fak","kurwa","shit"];
+const client = new Discord.Client();
+const users = [];
+var dates = Date();
+var timeh = dates;
+var timem = dates;
+const times = dates;
+var timessplit;
+var howerssplit;
+var czy = false;
+var czeste = false;
+var role;
+client.on("ready", () => {
+ console.log("I am ready!");
+    client.setInterval(function ()
+{
+     dates = Date();
+
+                                   timessplit = dates.split(" ");
+                                   howerssplit = timessplit[4].split(":");
+        
+
+
+                                   if(howerssplit[1] >=20 || howerssplit[1] < 4)
+                                       {
+                                           if (!czy)
+                                               {
+                                                   if(czeste)
+                                                       {
+                                                           role.edit({permissions:0}, "cisza nocna");
+                                                           czy = true;
+                                                           console.log("set 0");
+                                                       }
+                                               }
+                                       }
+                                    else{
+                                            if(czy)
+                                                {
+                                                    if(czeste)
+                                                        {
+                                                            role.edit({permissions:67584}, "cisza nocna");
+                                                            czy = false;
+                                                            console.log("set 67584");
+                                                        }
+                                                }
+                                        }
+}, 800);
+});
+client.on("message", (message) => {
+      // Assuming we mention someone in the message, this will return the user
+   if (message.content.startsWith('!setrole')) {
+        const user = message.mentions.roles.first();
+        // If we have a user mentioned
+        if (user) {
+            if(message.author.tag == "pythontest#9241")
+                {
+                    role = user;
+                    czeste = true;
+                    czy = true;
+                    message.reply("pomyślnie ustawiłem")
+                }
+            else
+                {
+                    message.reply("nie masz uprawnień")
+                }
+        } else {
+          message.reply('You didn\'t roles');
+        }
+   }
+  })
+
+client.login(auth.token);
