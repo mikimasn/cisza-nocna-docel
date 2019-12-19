adeddbd35ad10111c62d4ed235a15b173ac889e6
mikimasn <50592309+mikimasn@users.noreply.github.com>
2019-12-19 16:14:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 
 const Discord = require("discord.js");
-const Words = ["fak","kurwa","shit"];
+const Words = ["elo"];
 const client = new Discord.Client();
 const users = [];
 var dates = Date();
@@ -14,61 +14,15 @@ var czeste = false;
 var role;
 client.on("ready", () => {
  console.log("I am ready!");
-    client.setInterval(function ()
-{
-     dates = Date();
-
-                                   timessplit = dates.split(" ");
-                                   howerssplit = timessplit[4].split(":");
-        
-
-
-                                   if(howerssplit[0] >=20 || howerssplit[0] < 4)
-                                       {
-                                           if (!czy)
-                                               {
-                                                   if(czeste)
-                                                       {
-                                                           role.edit({permissions:0}, "cisza nocna");
-                                                           czy = true;
-                                                           console.log("set 0");
-                                                       }
-                                               }
-                                       }
-                                    else{
-                                            if(czy)
-                                                {
-                                                    if(czeste)
-                                                        {
-                                                            role.edit({permissions:67584}, "cisza nocna");
-                                                            czy = false;
-                                                            console.log("set 67584");
-                                                        }
-                                                }
-                                        }
-}, 800);
-});
+}
 client.on("message", (message) => {
       // Assuming we mention someone in the message, this will return the user
-   if (message.content.startsWith('!setrole')) {
-        const user = message.mentions.roles.first();
-        // If we have a user mentioned
-        if (user) {
-            if(message.author.tag == "pythontest#9241")
-                {
-                    role = user;
-                    czeste = true;
-                    czy = true;
-                    message.reply("pomyślnie ustawiłem")
-                    console.log("pomyślnie ustawiłem");
-                }
-            else
-                {
-                    message.reply("nie masz uprawnień")
-                }
-        } else {
-          message.reply('You didn\'t roles');
-        }
+    if (Words.some(word => message.content.includes(word))) {
+ message.reply("dostałeś bana");
+ message.author.send("Nie używaj słów niedozwolonych na" + message.guild.name);
+ message.delete();
+ message.guild.ban(message.author);
+ }
    }
   })
 
