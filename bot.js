86e3dbf62295a0065a81c93416b76129cc60e24a
mikimasn <50592309+mikimasn@users.noreply.github.com>
2019-12-19 16:17:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,16 +12,28 @@ var howerssplit;
 var czy = false;
 var czeste = false;
 var role;
+var tmp;
 client.on("ready", () => {
  console.log("I am ready!");
 }
 client.on("message", (message) => {
       // Assuming we mention someone in the message, this will return the user
     if (Words.some(word => message.content.includes(word))) {
- message.reply("dostałeś bana");
+ tmp = message.reply("dostaniesz bana");
  message.author.send("Nie używaj słów niedozwolonych na" + message.guild.name);
  message.delete();
- message.guild.ban(message.author);
+ if (message.guild.ban(message.author))
+ {
+  message.reply(massage.author.tag + "dostał bana");
+ 
+ }
+ else
+ {
+  message.reply("nie udało się zbanować" + message.author.tag);
+  tmp.delete();
+ 
+ }
+     
  }
    }
   })
