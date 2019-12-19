0ccf28b7b634c25b9a298ea9ffb3a7a413d319f1
mikimasn <50592309+mikimasn@users.noreply.github.com>
2019-12-19 16:50:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,6 @@ client.on("ready", () => {
 client.on("message", (message) => {
       // Assuming we mention someone in the message, this will return the user
     if (Words.some(word => message.content.includes(word))) {
-     console.log("dotarłem!");
      message.reply("dostaniesz bana");
      message.author.send("Nie używaj słów niedozwolonych na" + message.guild.name);
      message.delete();
