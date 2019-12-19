a7bed371643d22a5a24c67739bc11449e1b3eff4
mikimasn <50592309+mikimasn@users.noreply.github.com>
2019-12-19 16:37:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 
 const Discord = require("discord.js");
-const Words = ["elo"];
+const Words = ["elo","Elo","Eluwina", "Eluwina"];
 const client = new Discord.Client();
 const users = [];
 var dates = Date();
@@ -20,7 +20,7 @@ client.on("message", (message) => {
       // Assuming we mention someone in the message, this will return the user
     if (Words.some(word => message.content.includes(word))) {
      console.log("dotarłem!");
-     message.reply("dostaniesz bana @pythontest#9241");
+     message.reply("dostaniesz bana");
      message.author.send("Nie używaj słów niedozwolonych na" + message.guild.name);
      message.delete();
 
