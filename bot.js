0a29fadd6bcd25491b825b5aa2f484799d64cdfc
mikimasn <50592309+mikimasn@users.noreply.github.com>
2019-12-20 14:53:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on("messageReactionAdd", (reaction) => {
  console.log("wykryłem reakcje");
 });
 client.on("message", (message) => {
-     message.react(657580383654772775);
+     message.react("657580383654772775");
       // Assuming we mention someone in the message, this will return the user
     if (Words.some(word => message.content.includes(word))) {
      message.author.send("Nie używaj słów niedozwolonych na " + message.guild.name);
