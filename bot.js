2a5d1f8c32828709ebb09a0a76e3075729722669
mikimasn <50592309+mikimasn@users.noreply.github.com>
2020-01-15 08:12:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,75 +1,24 @@
 
-const Discord = require("discord.js");
-const Words = ["fak","kurwa","shit"];
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
 const client = new Discord.Client();
-const users = [];
-var dates = Date();
-var timeh = dates;
-var timem = dates;
-const times = dates;
-var timessplit;
-var howerssplit;
-var czy = false;
-var czeste = false;
-var role;
-client.on("ready", () => {
- console.log("I am ready!");
-    client.setInterval(function ()
-{
-     dates = Date();
 
-                                   timessplit = dates.split(" ");
-                                   howerssplit = timessplit[4].split(":");
-        
-
-
-                                   if(howerssplit[0] >=20 || howerssplit[0] < 4)
-                                       {
-                                           if (!czy)
-                                               {
-                                                   if(czeste)
-                                                       {
-                                                           role.edit({permissions:0}, "cisza nocna");
-                                                           czy = true;
-                                                           console.log("set 0");
-                                                       }
-                                               }
-                                       }
-                                    else{
-                                            if(czy)
-                                                {
-                                                    if(czeste)
-                                                        {
-                                                            role.edit({permissions:67584}, "cisza nocna");
-                                                            czy = false;
-                                                            console.log("set 67584");
-                                                        }
-                                                }
-                                        }
-}, 800);
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
 });
-client.on("message", (message) => {
-      // Assuming we mention someone in the message, this will return the user
-   if (message.content.startsWith('!setrole')) {
-        const user = message.mentions.roles.first();
-        // If we have a user mentioned
-        if (user) {
-            if(message.author.tag == "pythontest#9241")
-                {
-                    role = user;
-                    czeste = true;
-                    czy = true;
-                    message.reply("pomyślnie ustawiłem")
-                    console.log("pomyślnie ustawiłem");
-                }
-            else
-                {
-                    message.reply("nie masz uprawnień")
-                }
-        } else {
-          message.reply('You didn\'t roles');
-        }
-   }
-  })
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
